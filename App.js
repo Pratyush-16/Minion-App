3528a0399655dfcc48513cd5d3c66794212bcad3
Mona Lisa <pratyushsingh.1602@gmail.com>
2022-11-10 01:28:16 +0530

    functions/url added

--- App.js
+++ App.js
@@ -4,15 +4,32 @@ var outputDiv = document.querySelector("#output");
 
 //console.log(outputDiv)
 //outputDiv.innerText = "praty";
+ //var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+    var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+ function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text ;
+        }
+
+ function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with the server: try again after some time")
+        }
+
 
 function clickHandler() {
     
-    outputDiv.innerText= "hbjbjr " + txtInput.value;
-};
+    //outputDiv.innerText= "hbjbjr " + txtInput.value;
+    var inputText = txtInput.value; //taking input
 
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText= translatedText; //output
+    })
+    .catch(errorHandler);
+}
 
 btnTranslate.addEventListener("click", clickHandler)
-
-
-
-
